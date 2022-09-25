c66006801a7be993552c7c63e748e432e3a7c43a
Martín Morandini <84515114+MMorandiniGit@users.noreply.github.com>
2022-09-25 19:18:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express, { json } from 'express'
-import morgan  from 'morgan'
+import morgan from 'morgan'
 import 'dotenv/config'
 
 const app = express()
@@ -7,19 +7,36 @@ const app = express()
 app.use(json())
 app.use(morgan('dev'))
 
-const port = process.env.PORT 
+const port = process.env.PORT
 
 let lists = [
     {
-        name: 'Top Songs of the year',
-        description: 'The most listened songs of 2022'
+        name: 'Pop',
+        description: 'The best pop music in the world',
+        songs: [
+            {
+                title: 'late at night',
+                artist: 'Roddy Ricch',
+                albumName: 'LIVE LIFE FAST',
+                year: '2022'
+            },
+            {
+                title: 'BUTTERFLY EFFECT',
+                artist: 'Travis Scott',
+                albumName: 'ASTROWORLD',
+                year: '2022'
+            }
+        ]
     }
 ]
 
-let songs = []
-
 app.get('/lists', (req, res) => {
     res.send(lists)
+})
+
+app.post('/lists', (req, res) => {
+    lists.push(req.body)
+    res.send(res.body)
 })
 
 
